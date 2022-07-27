c7535f6e3cd066041b6b9190f43ea470cb68d39f
litsynp <nocte55is@gmail.com>
2022-07-27 10:27:58 +0900

    build: init project

---
+++ src/main/java/com/litsynp/flywaydemo/SpringFlywayDemoApplication.java
@@ -0,0 +1,12 @@
+package com.litsynp.flywaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFlywayDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFlywayDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/litsynp/flywaydemo/SpringFlywayDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.litsynp.flywaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlywayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
