7b636ca200afcc8637f062de2fca4d7e1047e6dc
litsynp <nocte55is@gmail.com>
2022-07-29 00:42:15 +0900

    refactor: rename main application

--- src/main/java/com/litsynp/flywaydemo/FlywayApplication.java
+++ src/main/java/com/litsynp/flywaydemo/FlywayApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringFlywayDemoApplication {
+public class FlywayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringFlywayDemoApplication.class, args);
+        SpringApplication.run(FlywayApplication.class, args);
     }
 }
